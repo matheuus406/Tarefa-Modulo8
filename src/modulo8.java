045d417021d9814200eebe463cec8653f758ee6b
matheus406 <matheussrodriguess406@gmail.com>
2023-06-30 07:42:42 -0300

    Initial commit

---
+++ src/modulo8.java
@@ -0,0 +1,16 @@
+public class modulo8 {
+    public static void main(String[] args) {
+        media();
+    }
+
+    public static void media() {
+        int nota1 = 8;
+        int nota2 = 6;
+        int nota3 = 9;
+        int nota4 = 5;
+
+        int mediaNota = (nota1 + nota2 + nota3 + nota4) /4;
+        System.out.println(mediaNota);
+
+    }
+}
